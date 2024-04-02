17be7ab9672db92529bfdac8cd744a4d15d45f96
nicoler717 <rosas.n@northeastern.edu>
2024-04-02 00:48:39 -0400

    fixed

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 const app = express()
 app.use(cors());
 app.use(express.json());
